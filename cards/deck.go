97239167b537bb85eefbffeeeed7519cd00a9e3a
Equipo Devops Sura Chile <6617328-devops_suracl@users.noreply.gitlab.com>
2021-01-14 16:39:55 -0300

    First commit

---
+++ cards/deck.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Creates a new type deck which is a slice of strings
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Club"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join(d, ",")
+}
